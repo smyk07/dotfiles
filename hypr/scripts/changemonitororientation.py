724db784fbeb28dac8648759938a6f6a3b9e6892
Samyak <samyak.bambole07@gmail.com>
2025-05-01 16:56:08 +0530

    updates [main]

--- hypr/scripts/changemonitororientation.py
+++ hypr/scripts/changemonitororientation.py
@@ -6,10 +6,10 @@ import shutil
 configs = {
     "dualh": "horizontal.conf",
     "dualv": "vertical.conf",
-    # "single": "single.conf",
+    "single": "single.conf",
 }
 
 shutil.copy(
     f"/home/clix/.config/hypr/scripts/{configs[sys.argv[1]]}",
-    "/home/clix/.config/hypr/monitors.conf",
+    "/home/clix/.config/hypr/monitors_windows_workspaces.conf",
 )
