698e80b16372ad6f44587612fd03b779273b4d7e
Samyak <samyak.bambole07@gmail.com>
2025-08-16 19:08:59 +0530

    updates

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -4,9 +4,19 @@ return {
   ft = { "org" },
   config = function()
     require("orgmode").setup({
-      org_agenda_files = "~/orgfiles/*.org",
+      org_agenda_files = "~/orgfiles/*/*.org",
       org_default_notes_file = "~/orgfiles/refile.org",
-      win_split_mode = "tabnew",
+      win_split_mode = "horizontal",
+      org_fold_enabled = false,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "org",
+      callback = function()
+        vim.opt_local.foldenable = false
+        vim.opt_local.foldlevel = 99
+        vim.opt_local.foldmethod = "manual"
+      end,
     })
   end,
 }
